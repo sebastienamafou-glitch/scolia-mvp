0a3a109a29115db2eeb10880c98d1f246ed112fe
sebastien Amafou <sebastien.amafou@gmail.com>
2025-11-23 15:07:57 +0100

    Feat: Activation du Portail Parents et correction de la route principale

--- scolia-frontend/src/main.tsx
+++ scolia-frontend/src/main.tsx
@@ -1,15 +1,15 @@
-// scolia-frontend/src/main.tsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter } from 'react-router-dom'; // Import du Router
+import { AuthProvider } from './contexts/AuthContext'; // Import du AuthProvider
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter> {/* <--- AJOUT CRUCIAL (Le contexte du Router) */}
+      <AuthProvider> {/* On s'assure que Auth est chargé en premier */}
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
