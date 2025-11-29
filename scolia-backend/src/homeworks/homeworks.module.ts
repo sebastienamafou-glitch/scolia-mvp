d4ddaf82848c3758b5052ab95060d51eca811df7
sebastien Amafou <sebastien.amafou@gmail.com>
2025-11-29 16:17:26 +0100

    Fix import ClassRepository

--- scolia-backend/src/homeworks/homeworks.module.ts
+++ scolia-backend/src/homeworks/homeworks.module.ts
@@ -1,12 +1,20 @@
+// scolia-backend/src/homeworks/homeworks.module.ts
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HomeworksService } from './homeworks.service';
 import { HomeworksController } from './homeworks.controller';
 import { Homework } from './entities/homework.entity';
 import { AuthModule } from '../auth/auth.module';
+// 👇 1. Import de l'entité Class
+import { Class } from '../classes/entities/class.entity'; 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Homework]), AuthModule],
+  imports: [
+    // 👇 2. Ajout de Class dans le forFeature
+    TypeOrmModule.forFeature([Homework, Class]), 
+    AuthModule
+  ],
   controllers: [HomeworksController],
   providers: [HomeworksService],
 })
