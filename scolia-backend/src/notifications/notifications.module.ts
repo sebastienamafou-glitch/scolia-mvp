7685412a7a77089b78c5b6c1a6ee81969d6c8f43
sebastien Amafou <sebastien.amafou@gmail.com>
2025-11-30 10:51:35 +0100

    Fix: Correction Affichage Modules et Sécurité Admin Dashboard (403)

--- scolia-backend/src/notifications/notifications.module.ts
+++ scolia-backend/src/notifications/notifications.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
-import { User } from '../users/entities/user.entity'; 
-import { Class } from '../classes/entities/class.entity'; 
+import { User } from '../users/entities/user.entity';
+import { Class } from '../classes/entities/class.entity';
+import { Notification } from './entities/notification.entity';
 
 @Module({
-  // Importe les entités User et Class pour que le service puisse les manipuler
-  imports: [TypeOrmModule.forFeature([User, Class])], 
+  imports: [
+    TypeOrmModule.forFeature([User, Class, Notification]) 
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
-  exports: [NotificationsService], 
+  exports: [NotificationsService],
 })
 export class NotificationsModule {}
